8b5c15d21b328eb74c655656659d730411e9e3dc
Layl <LaylConway@users.noreply.github.com>
2017-09-22 23:43:34 +0200

    Misc map changes

--- projects/rpg-game/assets/test_tiles.tsx
+++ projects/rpg-game/assets/test_tiles.tsx
@@ -1,4 +1,12 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="test_tiles" tilewidth="32" tileheight="32" tilecount="667" columns="23">
  <image source="test_tiles.png" width="736" height="928"/>
+ <tile id="299">
+  <objectgroup draworder="index">
+   <object id="1" x="21.1818" y="12.1364" width="20.1818" height="14.8181"/>
+  </objectgroup>
+ </tile>
+ <tile id="300">
+  <objectgroup draworder="index"/>
+ </tile>
 </tileset>
